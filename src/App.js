5a308e445c7be89559f4334227e177a36d51d9f4
saakeeb <saakeeb752@gmail.com>
2021-03-03 12:02:26 +0600

    complete basic search app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Fuse from 'fuse.js';
 import './App.css';
 
+import characters from './characters.json';
+
 function App() {
+  const [query, setQuery] = useState('')
+  const fuse = new Fuse(characters, {
+    keys: [
+      'name',
+      'company',
+      'species'
+    ],
+    includeScore: true
+  });
+
+  const onSearch = ({currentTarget}) =>{
+    setQuery(currentTarget.value);
+  }
+
+  const results = fuse.search(query);
+  const characterResults = query ? results.map(character => character.item): characters;
+
   return (
-    <div className="App">
+    <>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <h1>Futurama Characters</h1>
+        </div>
       </header>
-    </div>
+
+      <main className="container">
+        <ul className="characters">
+          {characterResults.map(character => {
+            const { name, company, species, thumb } = character;
+            return (
+              <li key={name} className="character">
+                <span className="character-thumb" style={{
+                  backgroundImage: `url(${thumb})`
+                }} />
+                <ul className="character-meta">
+                  <li>
+                    <strong>Name:</strong> {name}
+                  </li>
+                  <li>
+                    <strong>Company:</strong> {company}
+                  </li>
+                  <li>
+                    <strong>Species:</strong> {species}
+                  </li>
+                </ul>
+              </li>
+            )
+          })}
+        </ul>
+        <aside>
+          <form className="search">
+            <label>Search</label>
+            <input type="text" value={query} onChange={onSearch}/>
+          </form>
+        </aside>
+      </main>
+
+      <footer>
+        <div className="container">
+          <p>
+            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a> via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
 
